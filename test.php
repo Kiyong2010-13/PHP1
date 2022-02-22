a769cfde98c240d904eb538dfa49b170db36b81f
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-02-22 17:53:51 +0900

    test2

--- test.php
+++ test.php
@@ -2,5 +2,15 @@
 
 echo "Hello World";
 print("PHPを勉強中です！");
+print("I'm studying");
+print('I\'m studying"PHP"');
 
+?>
+
+<?php
+print(123 + 2 * 5 / 3);
+print("\n");
+print((123 + 2) * 5 / 3);
+print("123+2*5/3");
+print(60 * 60 * 24);
 ?>
