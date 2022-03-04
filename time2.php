98ec6800049f8125472a2cc782fd5bdba17fbb57
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-03-04 15:50:41 +0900

    time2.01

--- time2.php
+++ time2.php
@@ -2,5 +2,8 @@
 <?php
 $today = new DateTime('Asia/Tokyo');
 print('現在は' . $today->format('G:i:s') . 'です');
+print("\n");
+$today = new DateTime('Asia/Amman');
+print('現在は' . $today->format('G:i:s') . 'です');
 ?>
 </pre>
