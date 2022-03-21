a71b83a545a938608d65185a8b6fbf70048fa96c
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-03-21 15:38:37 +0900

    while3

--- while1.php
+++ while1.php
@@ -8,11 +8,18 @@ while ($i <= 365) {
 ?>
 <?php
 $i1 = 1;
-print($i1);
+print($i1 . "\n");
 $i1 =  $i1 + 1;
-print($i1);
+print($i1 . "\n");
 $i1 =  $i1 + 1;
-print($i1);
+print($i1 . "\n");
+?>
+<?php
+$i2 = 1;
+while ($i2 <= 365) {
+  print($i2);
+  $i2++;
+}
 ?>
 
 </pre>
