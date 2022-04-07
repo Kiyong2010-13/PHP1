2533eb87cd9bc35cb1fa886400c42ab3a3c7991a
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-04-07 15:42:00 +0900

    week1.2

--- week.php
+++ week.php
@@ -4,6 +4,7 @@ $week_name = ['日', '月', '火', '水', '木', '金', '土'];
 print('今日は、' . $week_name[date('w')] . '曜日です。');
 ?>
 <?php
-print(date('w'));
+$week_name2 = ['日', '月', ,'火', '水', '木', '金', '土'];
+print($week_name2[1]);
 ?>
 </pre>
