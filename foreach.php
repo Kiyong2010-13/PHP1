e7655c6f9e06547443139e50730b84194c639003
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-04-15 18:09:18 +0900

    foreach1.1

--- foreach.php
+++ foreach.php
@@ -14,6 +14,40 @@ foreach ($furits as $english => $japanese) {
 ?>
 <?php
 $furits2 = ['りんご', 'ぶどう', 'レモン', 'トマト', 'もも'];
-print ($furits2[2]);
+print ($furits2[2] . "\n");
+?>
+<?php
+$fruits3 = [
+  'apple' => 'りんご',
+  'greap' => 'ぶどう',
+  'lemon' => 'レモン',
+  'tomato' => 'トマト',
+  'peach' => 'もも', 
+];
+print($fruits3['lemon']. "\n");
+?>
+<?php
+$fruits4 = [
+  'apple' => 'りんご',
+  'greap' => 'ぶどう',
+  'lemon' => 'レモン',
+  'tomato' => 'トマト',
+  'peach' => 'もも',
+];
+foreach ($fruits4 as $english => $japanese){
+  print ($english . ':' . $japanese . "\n");
+}
+?>
+<?php
+$os = [
+  'win' => 'Windows',
+  'mac' => 'Macintosh',
+  'iphone' => 'iPhone',
+  'ipad' => 'iPad',
+  'android' => 'Android',
+];
+foreach ($os as $english => $fullname){
+  print ($english . ':' . $fullname . "\n");
+} 
 ?>
 </pre>
