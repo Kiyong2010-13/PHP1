9272a9a9edd040670ef0b5b37454d0a558d7fed4
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-05-06 21:28:46 +0900

    sprintf1.1

--- sprintf.php
+++ sprintf.php
@@ -1,6 +1,14 @@
 <pre>
 <?php
 $date = sprintf('%04d年 %02d月 %02d日', 2018, 1, 23);
-print($date);
+print($date . "\n");
+?>
+<?php
+$date2 = sprintf('%06d組', 1);
+print($date2 . "\n");
+?>
+<?php
+$date3 = sprintf('%07d代目 %sマン', 43333314, 'アンパン');
+print($date3 . "\n");
 ?>
 </pre>
