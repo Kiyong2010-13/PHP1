3a665705546ec69c9f34e1299c8b1a27ce6435ee
Kiyong2010-13 <k.shogo1994@gmail.com>
2022-05-23 16:16:00 +0900

    file_get_contents1.3

--- file_get_contents.php
+++ file_get_contents.php
@@ -1,9 +1,16 @@
 <pre>
 <?php
-$news = file_get_contents('../news_date/news.text');
+$news = file_get_contents('../news_date/news.txt');
 print($news);
 ?>
 <?php
+$news2 = file_get_contents('../news_date/news2.txt');
+print($news2);
+?>
+<?php
 readfile('../news_data/news.txt');
+?>
+<?php
+readfile('../news_data/news2.txt');
 ?>
 </pre>
